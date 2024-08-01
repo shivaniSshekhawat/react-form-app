b81d3706feec78b4c14659df9bb448d4ad1aa8c2
shivani.shekhawat <sshekhawat@initializ.io>
2024-08-01 15:49:18 +0530

    *test cases

---
+++ src/MyForm.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import {
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Checkbox,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+  FormHelperText,
+  Box,
+} from "@mui/material";
+
+const MyForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Box sx={{ mt: 4, maxWidth: 400, mx: "auto" }}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <FormControl fullWidth margin="normal">
+          <Controller
+            name="name"
+            control={control}
+            defaultValue=""
+            rules={{ required: "Name is required" }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Name"
+                error={!!errors.name}
+                helperText={errors.name ? errors.name.message : ""}
+              />
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: "Invalid email address",
+              },
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Email"
+                error={!!errors.email}
+                helperText={errors.email ? errors.email.message : ""}
+              />
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth margin="normal">
+          <InputLabel id="gender-label">Gender</InputLabel>
+          <Controller
+            name="gender"
+            control={control}
+            defaultValue=""
+            rules={{ required: "Gender is required" }}
+            render={({ field }) => (
+              <Select
+                {...field}
+                labelId="gender-label"
+                label="Gender"
+                error={!!errors.gender}
+              >
+                <MenuItem value="" disabled>
+                  <em>Select</em>
+                </MenuItem>
+                <MenuItem value="male">Male</MenuItem>
+                <MenuItem value="female">Female</MenuItem>
+                <MenuItem value="other">Other</MenuItem>
+              </Select>
+            )}
+          />
+          <FormHelperText error={!!errors.gender}>
+            {errors.gender ? errors.gender.message : ""}
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl component="fieldset" margin="normal">
+          <Controller
+            name="subscription"
+            control={control}
+            defaultValue=""
+            rules={{ required: "Subscription is required" }}
+            render={({ field }) => (
+              <RadioGroup {...field}>
+                <FormControlLabel
+                  value="free"
+                  control={<Radio />}
+                  label="Free"
+                />
+                <FormControlLabel
+                  value="premium"
+                  control={<Radio />}
+                  label="Premium"
+                />
+              </RadioGroup>
+            )}
+          />
+          <FormHelperText error={!!errors.subscription}>
+            {errors.subscription ? errors.subscription.message : ""}
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl margin="normal">
+          <Controller
+            name="agree"
+            control={control}
+            defaultValue={false}
+            rules={{ required: "You must agree to the terms" }}
+            render={({ field }) => (
+              <FormControlLabel
+                control={<Checkbox {...field} checked={field.value} />}
+                label="I agree to the terms and conditions"
+              />
+            )}
+          />
+          <FormHelperText error={!!errors.agree}>
+            {errors.agree ? errors.agree.message : ""}
+          </FormHelperText>
+        </FormControl>
+
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default MyForm;
---
+++ src/MyForm.test.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MyForm from "./MyForm";
+
+describe("MyForm", () => {
+  test("renders name input and validates", async () => {
+    render(<MyForm />);
+    const nameInput = screen.getByLabelText(/name/i);
+    expect(nameInput).toBeInTheDocument();
+
+    fireEvent.blur(nameInput);
+    const nameError = await screen.findByText(/name is required/i);
+    expect(nameError).toBeInTheDocument();
+  });
+
+  test("renders email input and validates", async () => {
+    render(<MyForm />);
+    const emailInput = screen.getByLabelText(/email/i);
+    expect(emailInput).toBeInTheDocument();
+
+    fireEvent.blur(emailInput);
+    const emailError = await screen.findByText(/email is required/i);
+    expect(emailError).toBeInTheDocument();
+
+    fireEvent.change(emailInput, { target: { value: "invalid-email" } });
+    fireEvent.blur(emailInput);
+    const emailInvalidError = await screen.findByText(/invalid email address/i);
+    expect(emailInvalidError).toBeInTheDocument();
+  });
+
+  test("renders gender dropdown and validates", async () => {
+    render(<MyForm />);
+    const genderSelect = screen.getByLabelText(/gender/i);
+    expect(genderSelect).toBeInTheDocument();
+
+    fireEvent.mouseDown(genderSelect);
+    const noneOption = await screen.findByRole("option", { name: /select/i });
+    expect(noneOption).toBeDisabled();
+
+    fireEvent.blur(genderSelect);
+    const genderError = await screen.findByText(/gender is required/i);
+    expect(genderError).toBeInTheDocument();
+
+    fireEvent.mouseDown(genderSelect);
+    const maleOption = await screen.findAllByRole("option", { name: /male/i });
+    fireEvent.click(maleOption[0]);
+
+    expect(genderSelect.value).toBe("male");
+  });
+
+  test("renders subscription radio buttons and validates", async () => {
+    render(<MyForm />);
+    const freeRadio = screen.getByLabelText(/free/i);
+    const premiumRadio = screen.getByLabelText(/premium/i);
+    expect(freeRadio).toBeInTheDocument();
+    expect(premiumRadio).toBeInTheDocument();
+
+    fireEvent.blur(freeRadio);
+    const subscriptionError = await screen.findByText(
+      /subscription is required/i
+    );
+    expect(subscriptionError).toBeInTheDocument();
+
+    fireEvent.click(freeRadio);
+    expect(freeRadio).toBeChecked();
+  });
+
+  test("renders agree checkbox and validates", async () => {
+    render(<MyForm />);
+    const agreeCheckbox = screen.getByLabelText(
+      /i agree to the terms and conditions/i
+    );
+    expect(agreeCheckbox).toBeInTheDocument();
+
+    fireEvent.blur(agreeCheckbox);
+    const agreeError = await screen.findByText(/you must agree to the terms/i);
+    expect(agreeError).toBeInTheDocument();
+
+    fireEvent.click(agreeCheckbox);
+    expect(agreeCheckbox).toBeChecked();
+  });
+
+  test("submits form with valid data", async () => {
+    render(<MyForm />);
+
+    const nameInput = screen.getByLabelText(/name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const genderSelect = screen.getByLabelText(/gender/i);
+    const freeRadio = screen.getByLabelText(/free/i);
+    const agreeCheckbox = screen.getByLabelText(
+      /i agree to the terms and conditions/i
+    );
+    const submitButton = screen.getByRole("button", { name: /submit/i });
+
+    fireEvent.change(nameInput, { target: { value: "User Name" } });
+    fireEvent.change(emailInput, {
+      target: { value: "UserEmail@example.com" },
+    });
+
+    fireEvent.mouseDown(genderSelect);
+    const maleOption = await screen.findAllByRole("option", { name: /male/i });
+    fireEvent.click(maleOption[0]);
+
+    fireEvent.click(freeRadio);
+    fireEvent.click(agreeCheckbox);
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() =>
+      expect(screen.queryByText(/name is required/i)).not.toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(screen.queryByText(/email is required/i)).not.toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(screen.queryByText(/gender is required/i)).not.toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(
+        screen.queryByText(/subscription is required/i)
+      ).not.toBeInTheDocument()
+    );
+    await waitFor(() =>
+      expect(
+        screen.queryByText(/you must agree to the terms/i)
+      ).not.toBeInTheDocument()
+    );
+  });
+});
